672c24b2daf6c74b8cfdf5c20d55cf60f3475de5
stepulans <elvis.stepulans@gmail.com>
2023-02-03 20:58:13 +0100

    Homework1

--- src/Homework/FirstHomwork/CaloriesOfPizza/CaloriesOfPizza.java
+++ src/Homework/FirstHomwork/CaloriesOfPizza/CaloriesOfPizza.java
@@ -7,7 +7,17 @@ public class CaloriesOfPizza {
 
         int radiusOfPizza1 = diameterOfPizza1 / 2;
         int radiusOfPizza2 = diameterOfPizza2 / 2;
-// Math.PI pi
+
+        double squareOfPizza1 = Math.PI * radiusOfPizza1 * radiusOfPizza1;
+        double squareOfPizza2 = Math.PI * radiusOfPizza2 * radiusOfPizza2;
+
+        double caloriesOfPizza1 = squareOfPizza1 * 40;
+        double caloriesOfPizza2 = squareOfPizza2 * 40;
+        System.out.println("Squere of small Pizza is " + squareOfPizza1);
+        System.out.println("Square of big Pizza is " + squareOfPizza2);
+        System.out.println("Total amount of calories in small Pizza is " + caloriesOfPizza1);
+        System.out.println("Total amount of calories in big Pizza is " + caloriesOfPizza2);
+
 
 
 
