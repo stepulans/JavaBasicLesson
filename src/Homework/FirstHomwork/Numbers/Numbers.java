1aa52e0675aa71cdbda6b12c5ce4a08428455c1b
stepulans <elvis.stepulans@gmail.com>
2023-02-03 21:12:03 +0100

    Homework1

--- src/Homework/FirstHomwork/Numbers/Numbers.java
+++ src/Homework/FirstHomwork/Numbers/Numbers.java
@@ -2,15 +2,13 @@ package Homework.FirstHomwork.Numbers;
 
 public class Numbers {
     public static void main(String[] args) {
-    int number = 256;
 
-    int number1 = number / 100;
-    System.out.println("First number of 256 is " +number1);
-    int number2 = (number / 10) % 10;
-    System.out.println("Second number of 256 is " +number2);
-    int number3 = number % 10;
-    System.out.println("Third number of 256 is " +number3);
-
-
+        int number = 256;
+        int number1 = number / 100;
+        System.out.println("First number of 256 is " +number1);
+        int number2 = (number / 10) % 10;
+        System.out.println("Second number of 256 is " +number2);
+        int number3 = number % 10;
+        System.out.println("Third number of 256 is " +number3);
 }
 }
