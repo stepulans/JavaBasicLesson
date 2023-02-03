0d24253bcc25b00d784db92fd8191962d9c71139
stepulans <elvis.stepulans@gmail.com>
2023-02-03 14:40:36 +0100

    Homework1

---
+++ src/Homework/FirstHomwork/CaloriesOfPizza/CaloriesOfPizza.java
@@ -0,0 +1,16 @@
+package Homework.FirstHomwork.CaloriesOfPizza;
+
+public class CaloriesOfPizza {
+    public static void main(String[] args) {
+        int diameterOfPizza1 = 24;
+        int diameterOfPizza2 = 28;
+
+        int radiusOfPizza1 = diameterOfPizza1 / 2;
+        int radiusOfPizza2 = diameterOfPizza2 / 2;
+// Math.PI pi
+
+
+
+
+    }
+}
