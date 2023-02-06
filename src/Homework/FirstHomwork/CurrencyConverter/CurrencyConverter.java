b31b136296b898c78f3af3b20b908c64df846355
stepulans <elvis.stepulans@gmail.com>
2023-02-06 09:35:56 +0100

    Homework1

--- src/Homework/FirstHomwork/CurrencyConverter/CurrencyConverter.java
+++ src/Homework/FirstHomwork/CurrencyConverter/CurrencyConverter.java
@@ -2,7 +2,7 @@ package Homework.FirstHomwork.CurrencyConverter;
 import java.util.Scanner;
 public class CurrencyConverter {
     public static void main(String[] args) {
-        System.out.println("1 Ruble");
+        System.out.println("1 Euro");
         System.out.println("2 Dollar");
 
         Scanner sc = new Scanner(System.in);
@@ -14,26 +14,26 @@ public class CurrencyConverter {
         double amount = sc.nextDouble();
         switch (choice) {
             case 1:
-                Ruble_to_Dollar(amount);
+                Euro_to_Dollar(amount);
                 break;
             case 2:
-                Dollar_to_Ruble(amount);
+                Dollar_to_Euro(amount);
                 break;
         }
 
     }
 
-    public static void Ruble_to_Dollar(double amt) {
-        System.out.println("1 Ruble = " + 0.013 + " Dollar");
+    public static void Euro_to_Dollar(double amt) {
+        System.out.println("1 Euro = " + 1.08 + " Dollar");
         System.out.println();
-        System.out.println(amt+" Ruble = " + (amt*0.013 + "Dollar"));
+        System.out.println(amt+"Euro = " + (amt*1.08 + "Dollar"));
         System.out.println();
 
     }
 
-    public static void Dollar_to_Ruble(double  amt) {
-        System.out.println("1 Dollar = " + 75.07 + " Ruble");
-        System.out.println(amt+"Dollar = " + (amt*75.07) + "Ruble");
+    public static void Dollar_to_Euro(double  amt) {
+        System.out.println("1 Dollar = " + 0.93 + " Euro");
+        System.out.println(amt+"Dollar = " + (amt*0.93) + "Euro");
 
     }
 }
