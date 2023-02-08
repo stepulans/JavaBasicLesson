816b5823edc29bb194cf53f0c97274011432bd05
stepulans <elvis.stepulans@gmail.com>
2023-02-08 12:01:53 +0100

    Lesson

---
+++ src/Lessons/lesson7/lessoncode/car/Manufacturer.java
@@ -0,0 +1,12 @@
+package Lessons.lesson7.lessoncode.car;
+
+public class Manufacturer {
+    String manufacturerFullName;
+    int year;
+
+    public Manufacturer(String manufacturer, int year) {
+        this.manufacturerFullName = manufacturer;
+        this.year = year;
+    }
+
+}
